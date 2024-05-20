eb369c21e1f0f3e49b12e99bbb89e77cea7a3633
Ste O'Brien <stephan.obrien@mcgill.ca>
2024-05-20 10:20:00 -0400

    docs(agn_pipe/analysis/spectral_analysis.py): adding doc strings

--- agn_pipe/analysis/spectral_analysis.py
+++ agn_pipe/analysis/spectral_analysis.py
@@ -5,7 +5,7 @@ from gammapy.datasets import (
     SpectrumDataset,
     SpectrumDatasetOnOff,
 )
-from gammapy.estimators import FluxPointsEstimator, LightCurveEstimator
+from gammapy.estimators import FluxPointsEstimator, LightCurveEstimator, FluxPoints
 
 # from gammapy.estimators.utils import resample_energy_edges
 from gammapy.makers import (
@@ -46,8 +46,25 @@ class SpectralAnalysis:
         source_name: str,
         tstart: Union[Time, str, None] = None,
         tstop: Union[Time, str, None] = None,
+        base_path: Optional[str] = "./analysis",
         scratch_path: Optional[str] = None,
     ):
+        """
+        Initializes the SpectralAnalysis object.
+
+        Args:
+            datastore_name : The name of the datastore.
+            ra : The right ascension of the source in degrees.
+            dec : The declination of the source in degrees.
+            source_name : The name of the source.
+            tstart : The start time for the analysis. Can be an astropy Time object, a string, or None.
+            tstop : The stop time for the analysis. Can be an astropy Time object, a string, or None.
+            base_path : The base path for the analysis. Defaults to "./analysis".
+            scratch_path : The scratch path for the analysis. If None, a unique path will be generated.
+
+        Returns:
+            None
+        """
 
         self.datastore_name = datastore_name
         self.ra = ra
@@ -59,19 +76,38 @@ class SpectralAnalysis:
         self.model_name = None
 
         if scratch_path is None:
-            self.scratch_path = Path("./" + str(uuid.uuid4()))
+            self.scratch_path = Path("./" + base_path + str(uuid.uuid4()))
         else:
-            self.scratch_path = Path(scratch_path)
+            self.scratch_path = Path(base_path + scratch_path)
 
         self.scratch_path.mkdir(exist_ok=True)
         self.aeff_max = 10.0
 
     def initialize_analysis(self):
+        """
+        Sets up datastore and analysis paths
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         self.setup_datastore()
         self.setup_analysis()
 
     def setup_datastore(self):
+        """
+        Setup the datastore
 
+        Creates a datastore from the inital directory passed. Gets the observation indices and observations.
+
+        Args:
+            None
+
+        Returns:
+            Noce
+        """
         self.datastore = DataStore.from_dir(self.datastore_name)
         self.obs_ids = query_datastore(
             self.datastore_name,
@@ -84,7 +120,17 @@ class SpectralAnalysis:
         self.observations = self.datastore.get_observations(self.obs_ids)
 
     def setup_fov(self):
+        """
+        Generate the exclusion regions for the FoV
 
+        Queries known stars and gamma-ray sources within the FoV.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         excl = get_exclusion_regions(self.ra, self.dec)
         self.on_exclusion_region = CircleSkyRegion(
             center=SkyCoord(self.ra, self.dec, unit="deg", frame="icrs"),
@@ -111,7 +157,18 @@ class SpectralAnalysis:
             self.exclusion_mask = ~geom.region_mask(self.regions)
 
     def setup_analysis(self):
+        """
+        Setups the initial gammapy analysis.
 
+        Creates required axes, geometries and dataset makers. This uses reflected region backgorund maker.
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        """
         self.target_position = SkyCoord(
             ra=self.ra, dec=self.dec, unit="deg", frame="icrs"
         )
@@ -134,7 +191,9 @@ class SpectralAnalysis:
         )
 
         dataset_maker = SpectrumDatasetMaker(
-            containment_correction=True, selection=["counts", "exposure", "edisp"]
+            containment_correction=True,
+            selection=["counts", "exposure", "edisp"],
+            use_region_center=False,
         )
         bkg_maker = ReflectedRegionsBackgroundMaker(exclusion_mask=self.exclusion_mask)
         safe_mask_masker = SafeMaskMaker(
@@ -160,7 +219,23 @@ class SpectralAnalysis:
         for dataset in self.datasets:
             dataset.write(filename=path / f"obs_{dataset.name}.fits.gz", overwrite=True)
 
-    def setup_model(self, model_type="pwl", model_name=None):
+    def setup_model(
+        self, model_type: Optional[str] = "pwl", model_name: Optional[str] = None
+    ):
+        """
+        Sets up the spectral model for the analysis.
+
+        Args:
+            model_type : The type of the spectral model. Can be "pwl" (power law), "lp" or "logpar" or "logparbola" (log parabola),
+                                    "expcut" or "cutoff" or "expontntial cutoff" (exponential cutoff). Defaults to "pwl".
+            model_name : The name of the model. If None, the source name will be used.
+
+        Raises:
+            ValueError: If the model_type is not found in the incorporated models.
+
+        Returns:
+            None
+        """
 
         pwl_models = ["pwl", "powerlaw"]
         logparabola_models = ["lp", "logpar", "logparbola"]
@@ -194,21 +269,45 @@ class SpectralAnalysis:
 
         if self.model_name is None:
             self.model_name = self.source_name
+        else:
+            self.model_name = model_name
 
         self.model = SkyModel(spectral_model=spectral_model, name=self.model_name)
         self.datasets.models = self.model
         self.dataset_stacked.models = self.model
 
-    def fit_spectrum(self):
+    def fit_spectrum(self) -> SkyModel:
+        """
+        Fits the spectral model to the stacked dataset.
 
+        The method runs the fit on the stacked dataset and then updates the models of the datasets with the results of the fit.
+
+        Args:
+            None
+        Returns:
+            SkyModel: A copy of the sky model after the fit.
+
+        """
         fit = Fit()
         fit.run(datasets=self.dataset_stacked)
         self.datasets.models = self.dataset_stacked.models.copy()
 
         return self.dataset_stacked.models.copy()
 
-    def get_spectral_points(self, e_min=0.1, e_max=30, n_bins=10):
+    def get_spectral_points(
+        self, e_min: float = 0.1, e_max: float = 10, n_bins: int = 10
+    ) -> FluxPoints:
+        """
+        Estimates the flux points of the fitted model.
 
+        Args:
+            e_min : The minimum energy for the flux points in TeV. Defaults to 0.1.
+            e_max : The maximum energy for the flux points in TeV. Defaults to 10.
+            n_bins : The number of energy bins for the flux points. Defaults to 10.
+
+        Returns:
+            FluxPoints: The estimated flux points.
+        """
         energy_edges = np.geomspace(e_min, e_max, n_bins) * u.TeV
 
         fpe = FluxPointsEstimator(
@@ -217,7 +316,23 @@ class SpectralAnalysis:
         self.flux_points = fpe.run(datasets=self.dataset_stacked)
         return self.flux_points
 
-    def run_lightcurve(self, duration, tstart=None, tstop=None):
+    def run_lightcurve(
+        self,
+        duration: float,
+        tstart: (Optional[Time]) = None,
+        tstop: (Optional[Time]) = None,
+    ) -> FluxPoints:
+        """
+        Runs a light curve estimation.
+
+        Args:
+            duration (float): The duration of each time bin for the light curve in days.
+            tstart (Optional[Time]): The start time for the light curve. If None, the start time of the analysis will be used.
+            tstop (Optional[Time]): The stop time for the light curve. If None, the stop time of the analysis will be used.
+
+        Returns:
+            LightCurve: The estimated light curve.
+        """
         if tstart is None:
             tstart = self.tstart
         if tstop is None:
